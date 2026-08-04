83bc3b05bad0cec69710951c98216b274af19a9f
wallradon <wallradon@gmail.com>
2026-08-05 00:36:09 +0700

    edit ignore

---
+++ admin/config.js
@@ -0,0 +1,11 @@
+/**
+ * Configuration Module
+ * ศูนย์กลางจัดการการตั้งค่า API และ Environment Variables 
+ */
+var CONFIG = {
+    // กำหนดค่า API Base URL สากลสำหรับโปรเจกต์นี้
+    API_BASE_URL: 'https://api-node-iot.onrender.com/api'
+};
+
+// ป้องกันการแก้ไขค่า CONFIG
+Object.freeze(CONFIG);
